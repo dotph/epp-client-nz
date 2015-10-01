1bd8798ef3684287a3d27666eea98f13a0db1e7c
Ealden Esto E. Escañan <ealden@escanan.com>
2015-10-01 17:55:15 +0800

    Optional authInfo for EPP::Domain::Info

--- test/domain/test_domain_info.rb
+++ test/domain/test_domain_info.rb
@@ -25,4 +25,25 @@ class TestEppDomainInfoCommand < Test::Unit::TestCase
       assert_equal 'example.com', xpath_find('//domain:name')
     end
   end
+
+  context 'EPP::Domain::Info with authInfo' do
+    setup do
+      @domain_info = EPP::Domain::Info.new('example.com', pw: '12345678')
+
+      @info    = EPP::Commands::Info.new(@domain_info)
+      @command = EPP::Requests::Command.new('ABC-123', @info)
+      @request = EPP::Request.new(@command)
+      @xml     = @request.to_xml
+
+      namespaces_from_request
+    end
+
+    should 'validate against schema' do
+      assert @xml.validate_schema(schema)
+    end
+
+    should 'set authInfo' do
+      assert_equal '12345678', xpath_find('//domain:authInfo/domain:pw')
+    end
+  end
 end
