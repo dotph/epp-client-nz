29ddc2d81520b49fd9a0771245b343bf758c53d4
br333 <bricefrancisco112@gmail.com>
2020-06-08 14:03:49 +0800

    Added response fee type

--- lib/epp-client/extension/fee/check_response.rb
+++ lib/epp-client/extension/fee/check_response.rb
@@ -11,19 +11,26 @@ module EPP
           raise ArgumentError, "no fee found for #{name}"
         end
 
+        def fee_type(name, command)
+          fees = get_fees command
+          return fees[name]['class'] if fees[name]
+
+          raise ArgumentError, "no fee type found for #{name}"
+        end
+
         protected
           def create_fees
             @create_fees = get_fees 'create'
           end
-            
+
           def renew_fees
             @renew_fees = get_fees 'renew'
           end
-        
+
           def transfer_fees
             @transfer_fees = get_fees 'transfer'
           end
-            
+
           def get_fees command
             @fees = {}
             fees_path = nodes_for_xpath('//fee:cd', @response.extension)
